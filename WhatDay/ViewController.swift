aac638ca029b9b527269ecd745911d31d054695b
brucelee2000 <brucelee2000@tp.edu.tw>
2015-01-02 21:23:49 -0800

    Add usage for "DatePicker" and "Alert" controller

--- WhatDay/ViewController.swift
+++ WhatDay/ViewController.swift
@@ -10,6 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    
+    @IBAction func displayDay(sender: UIButton) {
+        // Grab date from DatePicker
+        var chosenDate = datePicker.date
+        
+        // Create an NSDateFormatter
+        var formatter = NSDateFormatter()
+        formatter.dateFormat = "EEEE"
+        
+        // Grab the day and create a message
+        let day = formatter.stringFromDate(chosenDate)
+        let result = "That was a \(day)"
+        
+        // Create alert controller
+        let myAlert = UIAlertController(title: result, message: nil, preferredStyle: UIAlertControllerStyle.Alert)
+        
+        // Add actions to alert controller
+        myAlert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+        
+        // Show the alert
+        self.presentViewController(myAlert, animated: true, completion: nil)
+    }
+  
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
